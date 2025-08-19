7f638364dd364f0d005429eac44dc216249129c2
monda95 <jungmonda@naver.com>
2025-08-19 23:05:23 +0900

    meeting생성구현하기

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from fastapi.responses import ORJSONResponse
 
 from app.apis.v1.meeting_router import edgedb_router as meeting_router
 from app.apis.v1.meeting_router import mysql_router as mysql_router
-from app.configs.tortoise_cconfig import initialize_tortoise
+from app.configs.tortoise_config import initialize_tortoise
 
 app = FastAPI(default_response_class=ORJSONResponse)
 
