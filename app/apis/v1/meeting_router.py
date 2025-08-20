49eff53bdd3f3590a3df0ce1c23fda0ad635e034
monda95 <jungmonda@naver.com>
2025-08-20 13:06:31 +0900

    meeting 조회 api 생성

--- app/apis/v1/meeting_router.py
+++ app/apis/v1/meeting_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.dtos.create_meeting_response import CreateMeetingResponse
+from app.dtos.get_meeting_response import GetMeetingResponse
 
 # 실전에서는 db url 적지않을것. 강의에서만 사용하는것
 edgedb_router = APIRouter(prefix="/v1/edgedb/meetings", tags=["Meeting"], redirect_slashes=False)
@@ -21,3 +22,11 @@ async def api_create_meeting_edgedb() -> CreateMeetingResponse:
 )
 async def api_create_meeting_mysql() -> CreateMeetingResponse:
     return CreateMeetingResponse(url_code="abc")
+
+
+@mysql_router.get(
+    "/{meeting_url_code}",  # path variable
+    description="meeting 을 조회합니다.",
+)
+async def api_get_meeting_mysql(meeting_url_code: str) -> GetMeetingResponse:
+    return GetMeetingResponse(url_code="abc")
